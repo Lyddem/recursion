1bfa88d4562d2d665592857fd8e535455ba519a0
Lyddem <lydiademi@gmail.com>
2017-08-12 19:59:25 -0700

    First Commit

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,66 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	/*
+	{
+		key1: 'value1',
+		key2: 'value2'
+	}
+	*/
+
+	//loop through object, stringify every key and value!
+
+	// for(var key in obj){	
+	// 	key.toString();
+	// 	obj[key] = '' + obj[key2];
+	// }
+
+
+	if(typeof obj === '{}'){
+			return '' + obj;
+	}
+
+	if(typeof obj === 'boolean'){
+		return '' + obj;
+	}
+
+	if(typeof obj === 'string'){
+		return '' + string;
+	}
+
+	
+	if(Array.isArray(obj)){
+	  obj.forEach(element => {
+	      stringifyJSON(element)
+	  })
+    return '' + stringifyJSON(obj);
+	}
+
+	if(typeof obj === 'object'){
+
+			for(var key in obj){
+				key.toString();
+				obj[key] = ''  + obj[key];
+			}
+				return '' + obj;
+	}
+
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
